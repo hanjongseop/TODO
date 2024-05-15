bad92b6dd4a0b00db6181ce684987e1014d852b8
hanjongseop <whd4355@gmail.com>
2024-05-16 01:48:48 +0900

    update : css 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,111 +1,3 @@
-// import { useState } from "react";
-// import "./App.css";
-
-// function App() {
-//   const initialState = [
-//     // {id: 0, title: “”, body: “”, isDone: false}
-//   ];
-
-//   const [users, setUsers] = useState(initialState);
-//   const [newTitle, setNewTitle] = useState("");
-//   const [newBody, setNewBody] = useState("");
-
-//   const addUser = (e) => {
-//     e.preventDefault();
-
-//     if (!newTitle || !newBody) {
-//       alert("제목과 내용을 입력해주세요.");
-//       return;
-//     }
-
-//     setUsers([
-//       ...users,{id: Date.now(), title: newTitle, body: newBody, btn: false },
-//     ]);
-//   };
-
-//   const removeUser = (id) => {
-//     const updatedUser = users.filter((user) => user.id !== id);
-//     setUsers(updatedUser);
-//   };
-
-//   const toggleDone = (id) => {
-//     const updatedUsers = users.map((user) =>
-//       user.id === id ? { ...user, isDone: !user.isDone } : user
-//     );
-//     setUsers(updatedUsers);
-//   };
-
-//   const workingUsers = users.filter((user) => !user.isDone);
-
-//   const doneUsers = users.filter((user) => user.isDone);
-
-//   return (
-//     <>
-//       <h1>TODO</h1>
-//       <div>
-//         <form onSubmit={addUser}>
-//           제목
-//           <input
-//             // style={{ color: btn ? "#ffffff" : "#000000" }}
-//             className="title"
-//             type="text"
-//             // placeholder="제목"
-//             value={newTitle}
-//             onChange={(e) => setNewTitle(e.target.value)}
-//           />
-//           내용{" "}
-//           <input
-//             // style={{ color: btn ? "#ffffff" : "#000000" }}
-//             className="v"
-//             type="text"
-//             // placeholder="내용"
-//             value={newBody}
-//             onChange={(e) => setNewBody(e.target.value)}
-//           />
-//           <button type="submit">추가</button>
-//         </form>
-//         <h2>Working </h2>
-//         <ul>
-//           {workingUsers.map((user) => (
-//             <li key={user.id}>
-//               <span>
-//                 {user.title}
-//                 {user.body}
-//               </span>
-//               <button onClick={() => toggleDone(user.id)}>완료</button>
-//               <button onClick={() => removeUser(user.id)}>삭제</button>
-//             </li>
-//           ))}
-//         </ul>
-//         <h2>Done</h2>
-//         <ul>
-//           {doneUsers.map((user) => (
-//             <li key={user.id}>
-//               <span>
-//                 {user.title}
-//                 {user.body}
-//               </span>
-//               <button onClick={() => toggleDone(user.id)}>취소</button>
-//               <button onClick={() => removeUser(user.id)}>삭제</button>
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     </>
-//   );
-// }
-// export default App;
-
-// // <button className="cssAdd" onClick={() => switchHandler(user.id)}>
-// //   {user.btn ? " 취소" : "완료"}{" "}
-// // </button>;
-
-// // const switchHandler = (id) => {
-// //   setUsers(
-// //     users.map((user) => (user.id === id ? { ...user, btn: !user.btn } : user))
-// //   );
-// // };
-
 import { useState } from "react";
 import "./App.css";
 
@@ -123,7 +15,8 @@ function App() {
       alert("제목과 내용을 입력해주세요.");
       return;
     }
-
+    setNewTitle("");
+    setNewBody("");
     setTodoList([
       ...todoList,
       { id: Date.now(), title: newTitle, body: newBody, isDone: false },
@@ -148,7 +41,10 @@ function App() {
 
   return (
     <>
-      <h1>TODO</h1>
+      <header>
+        <h1>TODO</h1>
+      </header>
+
       <div>
         <form onSubmit={addTodo}>
           제목
@@ -158,23 +54,26 @@ function App() {
             value={newTitle}
             onChange={(e) => setNewTitle(e.target.value)}
           />
-          내용{" "}
+          내용
           <input
-            className="v"
+            className="content"
             type="text"
             value={newBody}
             onChange={(e) => setNewBody(e.target.value)}
           />
-          <button type="submit">추가</button>
+          <button className="add" type="submit">
+            추가
+          </button>
         </form>
         <h2>Working </h2>
         <ul>
           {workingTodos.map((todo) => (
             <li key={todo.id}>
-              <span>
-                {todo.title}
-                {todo.body}
-              </span>
+              <div className="input-text">
+                <p>제목:{todo.title}</p>
+                <p>내용:{todo.body}</p>
+              </div>
+
               <button onClick={() => toggleDone(todo.id)}>완료</button>
               <button onClick={() => removeTodo(todo.id)}>삭제</button>
             </li>
@@ -184,10 +83,11 @@ function App() {
         <ul>
           {doneTodos.map((todo) => (
             <li key={todo.id}>
-              <span>
-                {todo.title}
-                {todo.body}
-              </span>
+              <div className="input-text">
+                <p>제목:{todo.title}</p>
+                <p>내용:{todo.body}</p>
+              </div>
+
               <button onClick={() => toggleDone(todo.id)}>취소</button>
               <button onClick={() => removeTodo(todo.id)}>삭제</button>
             </li>
